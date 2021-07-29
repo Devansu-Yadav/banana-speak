070b247c30b4041379a0df74f12a101840ea0d6e
Devansu <devansuyadav@gmail.com>
2021-07-29 17:17:00 +0530

    Update:Implementation completed

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-alert("This is working!!");
+var input_btn = document.getElementById("input-text");
+var translate_btn = document.querySelector("#translate");
+var output_text = document.querySelector(".output #output-text");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getURL() {
+    return serverURL + "?" + "text=" + input_btn.value;
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+function clickEventHandler() {
+    fetch(getURL())
+    .then(response => response.json())
+    .then(data => output_text.innerText = "Translated text: " + data.contents.translated)
+    .catch(errorHandler);
+};
+
+translate_btn.addEventListener("click", clickEventHandler);
